a9d3c1ecd4833ad49d1b0a175e1cec67fb26c9ed
Luuk <luuk_wolferen@hotmail.com>
2019-11-19 12:30:12 +0100

    get employee, get employees added

--- UrenRegistratieQien/Repositories/EmployeeRepository.cs
+++ UrenRegistratieQien/Repositories/EmployeeRepository.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UrenRegistratieQien.Data;
 using UrenRegistratieQien.DatabaseClasses;
+using UrenRegistratieQien.Models;
 
 namespace UrenRegistratieQien.Repositories
 {
@@ -15,14 +17,61 @@ namespace UrenRegistratieQien.Repositories
         {
             this.context = context;
         }
-        public void GetEmployees()
+        public List<EmployeeModel> GetEmployees()
         {
+            List<EmployeeModel> employeeModelList = new List<EmployeeModel>();
+            foreach(IdentityUser employee in context.Users)
+            {
+                var employeeCasted = (Employee)employee;
+                var roleBridge = context.UserRoles.Single(p => p.UserId == employee.Id);
+                var roleName = context.Roles.Single(p => p.Id == roleBridge.RoleId).Name;
+
+                EmployeeModel newEmployee = new EmployeeModel
+                {
+                    EmployeeId = Convert.ToInt32(employee.Id),
+                    ClientId = employeeCasted.ClientId,
+                    FirstName = employeeCasted.FirstName,
+                    LastName = employeeCasted.LastName,
+                    Email = employeeCasted.Email,
+                    Address = employeeCasted.Address,
+                    Phone = employeeCasted.Phone,
+                    Role = roleName
+                };
+
+                employeeModelList.Add(newEmployee);
+
+            }
+
+            return employeeModelList;
+        }
+
+        public EmployeeModel GetEmployee(string id)
+        {
+            var employee = context.Users.Single(p => p.Id == id);
+
+            var employeeCasted = (Employee)employee;
+            var roleBridge = context.UserRoles.Single(p => p.UserId == id);
+            var roleName = context.Roles.Single(p => p.Id == roleBridge.RoleId).Name;
+
+            return new EmployeeModel
+            {
+                EmployeeId = Convert.ToInt32(employee.Id),
+                ClientId = employeeCasted.ClientId,
+                FirstName = employeeCasted.FirstName,
+                LastName = employeeCasted.LastName,
+                Email = employeeCasted.Email,
+                Address = employeeCasted.Address,
+                Phone = employeeCasted.Phone,
+                Role = roleName
+            };
+            
 
         }
 
         public void DeleteEmployee(string id)
         {
             var employee = context.Users.Single(p => p.Id == id);
+            
 
             foreach(DeclarationForm declarationForm in context.DeclarationForms)
             {
