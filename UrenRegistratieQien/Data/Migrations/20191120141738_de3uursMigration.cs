aa995c1a417e45ab467c1b96048781cbee1c8bad
KoenSchnaar <koenschnaar@gmail.com>
2019-11-20 16:29:22 +0100

    paar views verwijderd

---
+++ UrenRegistratieQien/Data/Migrations/20191120141738_de3uursMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UrenRegistratieQien.Data.Migrations
+{
+    public partial class de3uursMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
