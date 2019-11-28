3d14770720d5ccaa953091cda420af3a063ff853
Luuk <luuk_wolferen@hotmail.com>
2019-11-28 11:32:55 +0100

    foutje

--- UrenRegistratieQien/Controllers/MailserviceController.cs
+++ UrenRegistratieQien/Controllers/MailserviceController.cs
@@ -6,35 +6,116 @@ using System.Threading.Tasks;
 using MailKit;
 using MailKit.Net.Smtp;
 using MimeKit;
+using UrenRegistratieQien.Repositories;
+using UrenRegistratieQien.Models;
 
 namespace UrenRegistratieQien.Controllers
 {
     public class MailserviceController : Controller
     {
+        private readonly IDeclarationFormRepository declarationFormRepo;
 
-        public IActionResult MailService()
+        public MailserviceController(IDeclarationFormRepository DeclarationFormRepo)
         {
+            declarationFormRepo = DeclarationFormRepo;
+        }
+
+        [HttpPost]
+        public IActionResult MailService(DeclarationFormModel decModel, string uniqueId, string formId)
+        {
+
+            declarationFormRepo.EditDeclarationForm(decModel);
+            declarationFormRepo.SubmitDeclarationForm(decModel);
+
             var message = new MimeMessage();
 
-            message.From.Add(new MailboxAddress("Liza", "lizavanderkruk@gmail.com"));
+            message.From.Add(new MailboxAddress("Hans", "hanshanshans812@gmail.com"));
 
-            message.To.Add(new MailboxAddress("Koen", "koenschnaar@gmail.com"));
+            message.To.Add(new MailboxAddress("Luuk", "luuk_wolferen@hotmail.com"));
 
-            message.Subject = "Feestje";
+            message.Subject = "linktest";
             message.Body = new TextPart("plain")
             {
-                Text = "Ha Koen, dit mailtje is hopelijk aangekomen."
+                Text = "https://localhost:5001/Mailservice/ApproveOrReject/?uniqueId=" + uniqueId + "&formId=" + formId
             };
 
             using (var client = new SmtpClient())
             {
                 client.ServerCertificateValidationCallback = (s, c, h, e) => true;
                 client.Connect("Smtp.gmail.com", 587, false);
-                client.Authenticate("lizavanderkruk@gmail.com", "");
+                client.Authenticate("hanshanshans812@gmail.com", "Hans123!");
                 client.Send(message);
                 client.Disconnect(true);
             }
             return RedirectToRoute(new { controller = "Admin", action = "Admin" });
         }
+
+
+
+
+
+        public IActionResult MailService()
+        {
+
+
+            var message = new MimeMessage();
+
+            message.From.Add(new MailboxAddress("Hans", "hanshanshans812@gmail.com"));
+
+            message.To.Add(new MailboxAddress("Luuk", "luuk_wolferen@hotmail.com"));
+
+            message.Subject = "linktest";
+            message.Body = new TextPart("plain")
+            {
+                Text = "www.reddit.com"
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                client.Connect("Smtp.gmail.com", 587, false);
+                client.Authenticate("hanshanshans812@gmail.com", "Hans123!");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+            return RedirectToRoute(new { controller = "Admin", action = "Admin" });
+        }
+
+
+
+
+
+
+
+
+
+        public IActionResult ApproveOrReject(string uniqueId, string formId)
+        {
+
+            var formIdAsInt = Convert.ToInt32(formId);
+
+            if (declarationFormRepo.CheckIfIdMatches(uniqueId))
+            {
+                return View(declarationFormRepo.GetFormByFormId(formIdAsInt)); //pagina waar client kan regelen of het is goedgekeurd of niet
+            } else
+            {
+                return View("~/Views/Mailservice/ErrorUnknownId.cshtml"); // pagina waar staat unknown id
+            }
+        }
+
+        [HttpPost]
+        public IActionResult ApproveOrReject(string formId, bool approved)
+        {
+            //declarationFormRepo.ApproveOrReject(formId, approved)
+
+            //if approved:
+            // declarationFormRepo.ApproveForm(formId)
+            //else:
+            // declarationFormRepo.RejectForm(formId)
+
+            return View(); //yay het is gelukt nu kun je deze pagina sluiten
+        }
+
+
     }
 }
