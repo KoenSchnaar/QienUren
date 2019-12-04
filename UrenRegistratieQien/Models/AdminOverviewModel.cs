0744c065c35aca0180de43258d59763167e935f1
Liza <lizavanderkruk@gmail.com>
2019-12-04 14:48:29 +0100

    enum van status

--- UrenRegistratieQien/Models/AdminOverviewModel.cs
+++ UrenRegistratieQien/Models/AdminOverviewModel.cs
@@ -8,10 +8,16 @@ namespace UrenRegistratieQien.Models
     public class AdminOverviewModel
     {
         public Months SelectMonths { get; set; }
+        public Status SelectStatus { get; set; }
     }
+
 
     public enum Months
     {
         Januari, Februari, March, April, May, June, Juli, August, September, October, November, December
+    }
+    public enum Status 
+    {
+        Approved, Rejected, Pending
     }
 }
