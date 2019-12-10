4554f334b048a2a6be822588c433e411ce13763c
Luuk <luuk_wolferen@hotmail.com>
2019-12-10 14:26:38 +0100

    dagen van de week nu zichtbaar in formulier, weekenddagen zijn grijs

---
+++ UrenRegistratieQien/GlobalClasses/DayConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrenRegistratieQien.GlobalClasses
+{
+    public static class DayConverter
+    {
+        public static string DayToDag(string day)
+        {
+            switch (day)
+            {
+                case "Monday":
+                    return "Maandag";
+                case "Tuesday":
+                    return "Dinsdag";
+                case "Wednesday":
+                    return "Woensdag";
+                case "Thursday":
+                    return "Donderdag";
+                case "Friday":
+                    return "Vrijdag";
+                case "Saturday":
+                    return "Zaterdag";
+                case "Sunday":
+                    return "Zondag";
+                default:
+                    return "Geen geldige dag opgegeven";
+            }
+        }
+    }
+}
