ab07c78415d3a78e6ec2a4752569274806a8e6ca
Luuk <luuk_wolferen@hotmail.com>
2019-12-12 11:11:05 +0100

    zoeken is niet meer case sensitive

--- UrenRegistratieQien/Repositories/EmployeeRepository.cs
+++ UrenRegistratieQien/Repositories/EmployeeRepository.cs
@@ -88,10 +88,12 @@ namespace UrenRegistratieQien.Repositories
         public async Task<List<EmployeeModel>> GetAllAccounts(string searchString)
         {
             var AllEmployee = new List<EmployeeModel>();
-            var TempEmployee = await context.Employees.Where(p => p.Role != 1).ToListAsync();
+            var TempEmployee = await context.Employees.Where(p => p.Role != 1).OrderBy(x => x.FirstName).ToListAsync();
+            
             if (!(searchString == null))
             {
-                var filterEmployee = TempEmployee.Where(x => x.FirstName.Contains(searchString) || x.LastName.Contains(searchString) || (x.FirstName + ' ' + x.LastName).Contains(searchString)
+                searchString = searchString.ToLower();
+                var filterEmployee = TempEmployee.Where(x => x.FirstName.ToLower().Contains(searchString) || x.LastName.ToLower().Contains(searchString) || (x.FirstName.ToLower() + ' ' + x.LastName.ToLower()).Contains(searchString)
                    || searchString == null).OrderBy(x => x.FirstName);
                 foreach (var employee in filterEmployee)
                     AllEmployee.Add(EntityToEmployeeModel(employee));
